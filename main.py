3b38d78356c4758edff956cc2afdadfb6ac9e65e
Alejandr2106 <a.gonzalez48@uniandes.edu.co>
2024-10-09 22:59:09 -0500

    Avances

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
 from typing import Optional
 from DataModel import DataModel 
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hola World"}
+templates = Jinja2Templates(directory="templates")
 
-app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-   return {"item_id": item_id, "q": q}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+   return templates.TemplateResponse(request= request , name="index.html")
 
-@app.post("/datos/")
-async def recibir_datos(datos: DataModel):
-    return {"mensaje": "Datos recibidos", "datos": datos}
+@app.post("/predict", response_class=HTMLResponse)
+async def submit(request: Request, user_input: str = Form(...)):
+   return templates.TemplateResponse(request= request, name="indexResponse.html", context={"predict": "Sexo"})
+
+
+
+# app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#    return {"item_id": item_id, "q": q}
+
+# @app.post("/datos/")
+# async def recibir_datos(datos: DataModel):
+#     return {"mensaje": "Datos recibidos", "datos": datos}
